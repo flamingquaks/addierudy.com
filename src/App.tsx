32b6035a80dc544d5dcff066a61471deda56bfed
flamingquaks <developer@flamingquaks.dev>
2023-09-25 15:36:59 +0000

    Initial release of site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-// import Stack from '@mui/material/Stack';
-import Card from '@mui/material/Card'
 
-
-
-
+import ContactPhoneRoundedIcon from '@mui/icons-material/ContactPhoneRounded';
+import AlternateEmailRoundedIcon from '@mui/icons-material/AlternateEmailRounded';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 function App() {
 
@@ -15,10 +14,25 @@ function App() {
         <h1 className='title'>Hey, it's <span className='pop-text'>Addie Rudy!</span></h1>
         <p className='subtitle'>Welcome to my page!</p>
         <p className='subtitle'>BTW, my pronouns are <span className='white-text'>they/them</span></p>
-        <Card></Card>
+        <div className='lineItems'>
+          <div className='lineItem'>
+            <ContactPhoneRoundedIcon /> <a href="tel:+16786022545">+1 (678) 602-2545</a>
+          </div>
+          <div className='lineItem'>
+            <AlternateEmailRoundedIcon/><a href="mailto:addie@addierudy.com">addie@addierudy.com</a>
+          </div>
+          <div className='lineItem'>
+            <LinkedInIcon/><a href="https://www.linkedin.com/in/addierudy/" target='_blank'>addierudy</a>
+          </div>
+          <div className='lineItem'>
+            <DescriptionIcon/><a href="https://flamingquaks.com/">Personal Blog</a>
+          </div>
+        </div>
       </div>
     </>
   )
 }
 
 export default App
+
+
